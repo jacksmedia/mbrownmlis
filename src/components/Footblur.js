8940150ffefdb331f30496b7a3c3d3f18c539075
jacksmedia <alex@j4cks.com>
2021-10-19 13:39:54 -0700

    :art: footer art fix + new favicon
    huzzah!

--- src/components/Footblur.js
+++ src/components/Footblur.js
@@ -1,19 +1,13 @@
 import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
+import booksy from "../images/header.png"
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
-const Footblur = (width, height) => {
+
+const Footblur = () => {
   return(
-    <div className="footsie">
-      <StaticImage
-        className="tiny-icon"
-        layout="fixed"
-        formats={["AUTO", "WEBP", "AVIF"]}
-        src="./header.png"
-        width={399}
-        height={73}
-        quality={95}
-        alt="M Brown MLIS"
-      />
+    <div className="footer-img" alt="bookshelf picture copyright M Brown, 2013">
       <div className="floatybox">
         <p>M Brown, Masters in Library and Information Science</p>
         <p>Coded and designed by <a href="https://jacks.media">Jacks Media</a>
