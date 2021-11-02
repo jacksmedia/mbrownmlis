f6d2279861c721e936818c1a26087c4cfd8a28f0
jacksmedia <alex@j4cks.com>
2021-11-02 14:30:41 -0700

    newer footer text

--- src/components/Footblur.js
+++ src/components/Footblur.js
@@ -1,11 +1,10 @@
 import * as React from "react"
-import { StaticImage } from "gatsby-plugin-image"
 
 const Footblur = () => {
   return(
     <div className="footer-img" alt="bookshelf picture copyright M Brown, 2013">
       <div className="floatybox">
-        <span>M Brown, Masters in Library and Information Science</span>
+        <span>M Brown, Master of Library and Information Science</span>
         <span>Coded and designed by <a href="https://jacks.media">Jacks Media</a>
         {`, `}{new Date().getFullYear()}</span>
       </div>
