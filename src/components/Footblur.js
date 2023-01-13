38471888870217d778b9c39242fa12e360e65cbd
jacksmedia <alex@j4cks.com>
2023-01-12 16:50:11 -0800

    :wrench: year now perpetually current

--- src/components/Footblur.js
+++ src/components/Footblur.js
@@ -1,13 +1,15 @@
 import * as React from "react"
 
+const Jahr = new Date().getFullYear()
+
 const Footblur = () => {
   return(
     <div className="footer-img" alt="bookshelf picture copyright M Brown, 2013">
       <div className="floatybox">
         <span>M Brown, Master of Library and Information Science</span>
         <span>Coded and designed by <a href="https://jacks.media">Jacks Media</a>
-        {`, `}{new Date().getFullYear()}</span>
-        <span>All images copyright M Brown, 2015-2022.</span>
+        {`, `}{Jahr}</span>
+        <span>All images copyright M Brown, 2015-{Jahr}.</span>
       </div>
     </div>
   )
